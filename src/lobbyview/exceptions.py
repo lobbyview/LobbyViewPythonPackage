b554cf105291ce62e59d4ab963e109116a85c00e
Rodrigo <rgg1@mit.edu>
2024-05-01 00:45:40 -0400

    added some suggestions from code review, including making the exceptions.py file, shortening paginate doctest, adding status_code as exception attribute, adding support for mix and max parameters for some methods

---
+++ src/lobbyview/exceptions.py
@@ -0,0 +1,64 @@
+"""
+Subclasses of LobbyViewError that represent different types of errors.
+"""
+from .LobbyView import LobbyViewError
+
+class UnauthorizedError(LobbyViewError):
+    """
+    Raised when the API token is invalid or unauthorized.
+    """
+    def __init__(self, status_code=None):
+        super().__init__()
+        self.status_code = status_code
+    def __str__(self):
+        if self.status_code is not None:
+            return f"Unauthorized, status code: {self.status_code}. Please check your API token and permissions."
+        return "Unauthorized. Please check your API token and permissions."
+
+class TooManyRequestsError(LobbyViewError):
+    """
+    Raised when the API rate limit is exceeded.
+    """
+    def __init__(self, status_code):
+        super().__init__()
+        self.status_code = status_code
+    def __str__(self):
+        return f"Rate limit exceeded, status code: {self.status_code}"
+
+class PartialContentError(LobbyViewError):
+    """
+    Raised when the API returns a partial response.
+    """
+    def __init__(self, status_code):
+        super().__init__()
+        self.status_code = status_code
+    def __str__(self):
+        return f"Partial content returned, status code: {self.status_code}"
+
+class UnexpectedStatusCodeError(LobbyViewError):
+    """
+    Raised when the API returns an unexpected status code.
+    """
+    def __init__(self, status_code):
+        super().__init__()
+        self.status_code = status_code
+    def __str__(self):
+        return f"Unexpected status code: {self.status_code}"
+
+class InvalidPageNumberError(LobbyViewError):
+    """
+    Raised when the current page number is greater than the total number of pages.
+    """
+    def __init__(self, current_page, total_pages):
+        super().__init__()
+        self.current_page = current_page
+        self.total_pages = total_pages
+    def __str__(self):
+        return f"Invalid page number: {self.current_page}, total pages: {self.total_pages}"
+
+class RequestError(LobbyViewError):
+    """
+    Raised when an error occurs during the request to the LobbyView API.
+    """
+    def __init__(self):
+        super().__init__()
